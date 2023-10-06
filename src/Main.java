be18e4c0a0d454cf7ea39f2135a6762ecb54ef99
Anna Gryazeva <anna_gryazeva@mail.ru>
2023-10-06 22:44:10 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 500;
-       int replenishmentAccount = 1500;
+        int replenishmentAccount = 1500;
 
-       int bonus = 0;
-       if (replenishmentAccount > 1000) {
-           bonus = replenishmentAccount / 100;
-       }
+        int bonus = 0;
+        if (replenishmentAccount > 1000) {
+            bonus = replenishmentAccount / 100;
+        }
 
-       int finalBalance = initialBalance + replenishmentAccount + bonus;
+        int finalBalance = initialBalance + replenishmentAccount + bonus;
         System.out.println("Итог: " + finalBalance);
         System.out.println("Бонус: " + bonus);
-
 
 
     }
